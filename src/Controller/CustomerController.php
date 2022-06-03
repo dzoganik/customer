a713fb9efb0b952658c4896d292b727c9b3dffab
Ján Džoganík <jan.dzoganik@quanti.cz>
2022-06-03 22:38:58 +0200

    Added: customers common features

---
+++ src/Controller/CustomerController.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use App\Form\Type\CustomerType;
+use App\Repository\CustomerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomerController extends AbstractController
+{
+    #[Route('/', name: 'app_customer')]
+    public function index(
+        Request $request,
+        CustomerRepository $customerRepository
+    ): Response {
+        $customer = new Customer();
+        $form = $this->createForm(CustomerType::class, $customer);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $customer = $form->getData();
+            $customerRepository->add($customer, true);
+
+            return $this->redirectToRoute('app_customer');
+        }
+
+        return $this->renderForm('customer/list.html.twig', [
+            'form' => $form,
+            'customers' => $customerRepository->findAll(),
+        ]);
+    }
+}
---
+++ src/Entity/Customer.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Customer
+ * @package App\Entity
+ */
+#[ORM\Entity(repositoryClass: CustomerRepository::class)]
+class Customer
+{
+    /**
+     * @var int
+     */
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string', length: 60)]
+    private $firstName;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string', length: 60)]
+    private $lastName;
+
+    /**
+     * @var string
+     */
+    #[ORM\Column(type: 'string', length: 30)]
+    private $phoneNumber;
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return $this
+     */
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return $this
+     */
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    /**
+     * @param string $phoneNumber
+     * @return $this
+     */
+    public function setPhoneNumber(string $phoneNumber): self
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+}
